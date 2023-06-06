e5da5cfc13e399791ce6bcce528d1394daa02c3d
Pazific <advolatum@yandex.ru>
2023-06-06 18:24:07 +0300

    refactor: edit db uri

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ const {
   NODE_ENV = 'production',
   JWT_SECRET,
   PORT = 3000,
-  DB_URI = 'mongodb://localhost:27017/mestodb',
+  DB_URI = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
 
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : '8ec676b235957e7aec559dfe8e8513bc9b73ff0d5e03ee4865ebfea5f0b93c52';
