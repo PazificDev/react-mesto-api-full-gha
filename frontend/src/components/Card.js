f3fade636ecc1059a8a4cf9983b02948f391108a
Pazific <advolatum@yandex.ru>
2023-06-06 21:38:46 +0300

    refactor: repair heart like color changing

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -5,7 +5,7 @@ function Card(props) {
 
   const currentUser = useContext(CurrentUserContext);
   const isOwn = props.card.owner === currentUser._id;
-  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+  const isLiked = props.card.likes.some(i => i === currentUser._id);
 
   const cardLikeButtonClassName = ( 
     `elements__like-button ${isLiked && 'elements__like-button_active'}` 
