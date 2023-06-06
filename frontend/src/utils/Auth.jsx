8948d5b5fa7753bb8cfe75ea8973964a1766794d
Pazific <advolatum@yandex.ru>
2023-06-06 18:45:44 +0300

    refactor: methods of Api

--- frontend/src/utils/Auth.jsx
+++ frontend/src/utils/Auth.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.pazificdev.mesto.nomoredomains.rocks';
+export const BASE_URL = 'http://api.pazificdev.mesto.nomoredomains.rocks';
 
 export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
